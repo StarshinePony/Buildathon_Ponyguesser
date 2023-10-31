30745678c6352008c087b11556fa99ca2acec289
StarshinePony <128289655+StarshinePony@users.noreply.github.com>
2023-10-31 20:10:20 +0100

    i like trees

---
+++ main.py
@@ -0,0 +1,93 @@
+from modules.ponyguesser import guesser
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+import json
+import asyncio
+
+load_dotenv()
+# Get the API token from the .env file.
+DISCORD_TOKEN = os.getenv("discord_token")
+authserver = os.getenv("authserver")
+developer = os.getenv("developerid")
+
+# import all of the cogs
+intents = discord.Intents.all()
+currentprefix = 'v!'
+bot = commands.Bot(command_prefix=currentprefix, intents=intents)
+
+# register the class with the bot
+bot.remove_command("help")
+
+
+@bot.event
+async def on_ready():
+    await bot.add_cog(guesser(bot))
+    await bot.tree.sync(guild=discord.Object(id=authserver))
+    await bot.tree.sync()
+    print(f"Synced commands")
+    print("[MAIN INFO] Bot is ready!")
+@bot.event
+async def on_guild_join(guild):
+    if guild.me == bot.user:    
+        for channel in guild.text_channels:
+            if channel.permissions_for(guild.me).send_messages:
+                print("[MAIN INFO]: Bot joined new guild!")
+                await channel.send("Thanks for adding Trixie-Bot to your server! "
+                                   "First of all, run the t!setup command to set up all your roles and channels!")
+
+                break
+@bot.event
+async def on_member_join(member):
+    server_id = member.guild.id
+
+    with open('setup_data.json', 'r') as file:
+        setup_data = json.load(file)
+        if setup_data:
+            member_role_id = setup_data.get(
+                str(server_id), {}).get("member_role_id")
+            member_role = discord.utils.get(
+                member.guild.roles, id=member_role_id)
+            # Send the message to the mod_channel
+            await member.add_roles(member_role)
+            print("[Main INFO]: Member Role given")
+        else:
+            print("[Main ERROR]: No Setup Data available! No role was handed out!")
+    with open('setup_data.json', 'r') as file:
+        setup_data = json.load(file)
+        if setup_data:
+            new_pony_role = setup_data.get(
+                str(server_id), {}).get("new_pony_role_id")
+            new_pony_role = discord.utils.get(
+                member.guild.roles, id=new_pony_role)
+            await member.add_roles(new_pony_role)
+            await asyncio.sleep(6000)
+            await member.remove_roles(new_pony_role)
+
+@bot.tree.context_menu(name="whothis")
+async def whothis(interaction: discord.Interaction, member: discord.Member):
+    embed = discord.Embed(title=f"{member.name}", description=f" {member.id}")
+    embed.add_field(name="Joined Discord", value=member.created_at.strftime("%d/%m/%Y/%H:%M:%S"), inline=False)
+    embed.add_field(name="Roles", value=", ".join([role.mention for role in member.roles]), inline=False)
+    embed.add_field(name="Badges", value=", ".join([badge.name for badge in member.public_flags.all()]), inline=False)
+    embed.add_field(name="Activity", value=member.activity, inline=False)
+    embed.set_thumbnail(url=member.avatar.url)
+    await interaction.response.send_message(embed=embed)
+@bot.event
+async def on_message(message):
+    # Ignore messages sent by bots to avoid command loops
+    if message.author.bot:
+        return
+
+    # Process commands in DM channels but do not add entries to the spam counter.
+    if isinstance(message.channel, discord.DMChannel):
+        await bot.process_commands(message)
+        return
+
+    # Process commands in guild channels
+    await bot.process_commands(message)
+
+
+# start the bot with our token
+bot.run(DISCORD_TOKEN)
---
+++ modules/ponyguesser.py
@@ -0,0 +1,74 @@
+import discord
+from discord.ext import commands
+import requests
+import asyncio
+from fuzzywuzzy import fuzz
+import json
+
+class guesser(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        with open('episodes.json', 'r') as file:
+            self.episodes_data = json.load(file)
+
+    @commands.hybrid_command(name='ponyguesser', description="Play Some Pony Guesser")
+    async def guess_image(self, ctx):
+        try:
+            api_url = 'https://ponyguessr.com/api/resource/gen?runId&subtitles=false&audioLength=10&resourceType=frame'
+
+            response = requests.get(api_url)
+            data = response.json()
+
+            if 'id' in data:
+                image_id = data['id']
+                image_url = f'https://ponyguessr.com/api/resource/get/{image_id}.png'
+                await ctx.send(image_url)
+                await ctx.send(f'Guess the episode by typing the episode name or something similar!')
+
+                def check(msg):
+                    return msg.author == ctx.author and msg.channel == ctx.channel
+
+                try:
+                    user_guess = await self.bot.wait_for('message', check=check, timeout=500)
+                    user_input = user_guess.content
+
+                    max_ratio = 0
+                    matched_episode = ""
+                    matched_season = ""
+                    matched_episode_number = ""
+
+                    for episode_name, episode_code in self.episodes_data['episodes'].items():
+                        ratio = fuzz.token_set_ratio(user_input, episode_name)
+                        if ratio > max_ratio:
+                            max_ratio = ratio
+                            matched_episode = episode_code
+                            matched_season, matched_episode_number = episode_code.split('E')
+                            trash, matched_season_number = matched_season.split('S')
+
+                    if max_ratio > 90:  # Adjust the similarity threshold as needed
+                        check_url = f'https://ponyguessr.com/api/resource/{image_id}/check?season={matched_season_number}&episode={matched_episode_number}'
+                        check_response = requests.get(check_url)
+                        check_data = check_response.json()
+
+                        if check_data['correct']:
+                            await ctx.send(f'Congratulations! You guessed correctly. It is from Season {matched_season}, Episode {matched_episode_number}.')
+                        else:
+                            await ctx.send(f'Sorry, your guess was incorrect.(Season: {matched_season_number}, Episode: {matched_episode_number})')
+                            await ctx.send(f'The correct episode is from Season {check_data["season"]}, Episode {check_data["episode"]}.')
+                    else:
+                        await ctx.send("Sorry, couldn't find a matching episode. Please try again.")
+
+                except asyncio.TimeoutError:
+                    await ctx.send('Time is up! Please try again.')
+
+            else:
+                await ctx.send('Unable to fetch image ID')
+
+        except Exception as e:
+            print(f'Error occurred: {e}')
+            await ctx.send('Error fetching image')
+
+
+def setup(bot):
+    bot.add_cog(guesser(bot))
